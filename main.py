04ad9b262ee43ec4a76d015757cd96894589f610
foopy02 <88570123+foopy02@users.noreply.github.com>
2021-12-07 19:01:36 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+
+from os import startfile
+import pornhub_api
+from pornhub_api import PornhubApi
+from pornhub_api.backends.aiohttp import AioHttpBackend
+from bs4 import BeautifulSoup
+from video import Video
+import requests
+import pprint
+
+VIEW_VIDEO_PATH = "https://www.pornhub.com/view_video.php?="
+
+def start():
+    api = PornhubApi()
+    data = api.search.search(
+    "russian",
+    ordering="hot",
+    period="weekly",
+    tags=['black']
+    )
+    list_of_videos = []
+    counter = 1
+    for vid in data.videos:
+        print(str(counter) + "/"+str(len(data.videos)) + vid.title)
+        counter = counter + 1
+        upVotes, downVotes, author = get_more_info_of_video(vid.video_id)
+        video = Video(
+            id=vid.video_id,
+            title=vid.title,
+            url=VIEW_VIDEO_PATH + vid.video_id,
+            length=convert_to_time(vid.duration),
+            histogram=None,
+            tags=get_lists_of_tags(vid.tags),
+            category=get_list_of_categories(vid.categories),
+            views = vid.views,
+            dateAdded=vid.publish_date,
+            dateUpdated=None,
+            up_votes=upVotes,
+            down_votes=downVotes,
+            author=author
+        )
+        list_of_videos.append(video)
+    for video in list_of_videos:
+        print(video.__dict__)
+
+
+def get_lists_of_tags(list_of_tags):
+    result = []
+    for tag in list_of_tags:
+        result.append(tag.tag_name)
+    return result
+
+def get_list_of_categories(list_of_categories):
+    result = []
+    for category in list_of_categories:
+        result.append(category.category)
+    return result
+
+def get_more_info_of_video(id):
+    req = requests.get(VIEW_VIDEO_PATH + id)
+    with open("C://Users//Адиль//Desktop//Job//result.txt", "r", encoding="utf-8") as ss:
+        html = ss.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    votesUp = soup.find("span", {"class": "votesUp"}).get('data-rating')
+    votesDown = soup.find("span", {"class": "votesDown"}).get('data-rating')
+    author = soup.find("div",{"class": "userInfo"}).find("span",{"class":"usernameBadgesWrapper"}).get_text()
+    return votesUp, votesDown, author
+
+def convert_to_time(length):
+    splitted = length.split(':')
+    minutes = int(splitted[0])
+    seconds = int(splitted[1])
+    result = (minutes * 60) + seconds
+    return result
+
+
+#Possible ordering words: #mostviewed, newest, rating, featured
+#weekly, monthly, alltime
+if __name__=="__main__":
+    start()
---
+++ video.py
@@ -0,0 +1,16 @@
+class Video:
+    def __init__(self, id, title, url, length, histogram, tags, category, views, dateAdded, dateUpdated, up_votes, down_votes, author):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.length = length
+        self.histogram = histogram
+        self.tags = tags
+        self.category = category
+        self.views = views
+        self.dateAdded = dateAdded
+        self.dateUpdated = dateUpdated
+        self.up_votes = up_votes
+        self.down_votes = down_votes
+        self.author = author
+        
