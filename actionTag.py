70d9e4c464d96b0e54c5631cf5ac94d00e023ff9
foopy02 <88570123+foopy02@users.noreply.github.com>
2021-12-14 23:44:10 +0600

    Add files via upload

---
+++ actionTag.py
@@ -0,0 +1,8 @@
+class ActionTag:
+    def __init__(self, videoId, videoUrl, author, dataTagName, dataTagTime ):
+        self.videoId = videoId
+        self.videoUrl = videoUrl
+        self.author = author
+        self.dataTagName = dataTagName
+        self.dataTagTime = dataTagTime
+        
