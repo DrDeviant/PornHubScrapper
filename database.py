ceed79aef145af3ad4a6feb7336825e87b319ba1
foopy02 <88570123+foopy02@users.noreply.github.com>
2021-12-18 23:35:30 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
+# mongoClient = pymongo.MongoClient("mongodb+srv://admin:RaspberryPi19!@cluster0.ne4mv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 import pymongo 
-mongoClient = pymongo.MongoClient("mongodb+srv://admin:RaspberryPi19!@cluster0.ne4mv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoClient = pymongo.MongoClient("mongodb+srv://admin:RaspberryPi19!@cluster0.lrodn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 ActionTags = mongoClient['ActionTags']
 Pornstars = mongoClient['PornStars']
 def save_to_db(actionTag):
     ActionTags[actionTag['dataTagName']].insert_one(actionTag)
 def save_pornstars(pornstar):
     Pornstars['PornStars'].insert_one(pornstar)
+
+
 
 
